687ccf1924ce6fbef65de207d2548ba8edc629b0
bonetrail <moorbren@oregonstate.edu>
2018-11-30 19:18:23 -0800

    Item stuff

--- server.js
+++ server.js
@@ -13,19 +13,19 @@ var mongoClient = require('mongodb').MongoClient;
 var mongoHost = process.env.MONGO_HOST;
 var mongoPort = process.env.MONGO_PORT || 27017;
 var mongoUser = process.env.MONGO_USER;
-var mongoPassword = process.env.MONGO_USER;
-var mongoDBName = process.env.MONGO_DB_NAME;
-var mongoURL =
-	'mongodb://' + mongoUser + ':' + mongoPassword + '@' +
-  mongoHost + ':' + mongoPort + '/' + mongoDBName;
-  
+
+var mongoURL = 'mongodb://' + mongoUser + ':' + mongoUser + '@' +
+  mongoHost + ':' + mongoPort + '/' + mongoUser;
+
 var db;
 
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-var availablePages = ['home', 'store', 'supplies', 'crafting']; 
+var availablePages = ['home', 'store', 'supplies', 'crafting'];
+var itemList;
+var collectionsNeeded = [[], ["items"], [], []];
 
 app.use(express.static('public'));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -38,13 +38,18 @@ app.get('/', function(req, res, next){
 
 app.get('/:section', function(req, res, next){
   console.log("==incoming request-URL::", req.url);
-  if (availablePages.indexOf(req.params.section) > -1) {
+  var pageId = availablePages.indexOf(req.params.section);
+  if (pageId > -1) {
     console.log("==section found::"+req.params.section);
-    res.status(200).render(req.params.section);
+    var collections = [];
+    if(collectionsNeeded[pageId].length > 0)
+        collections.items = itemList;
+        console.log(collections.items);
+    res.status(200).render(availablePages[pageId], collections)
   }
   else {
     console.log("==404 section not found::"+req.params.section);
-    next(); 
+    next();
   }
 });
 
@@ -59,20 +64,19 @@ var tickTime = 2500; //milliseconds between "frames"
 
 //The server-side logic will happen inside this function.
 function tickServer() {
-  
+
   //deltaTime calculations
   //realistically, I'm not sure how much the milliseconds in deltaTime is really going to matter, but it's generally a good idea to put that in game loops in case you ever need it.
   var newTime = Date.now();
   var deltaTime = newTime - time;
   time = newTime;
   console.log("deltaTime:: ", deltaTime);
-
   /* SERVER-SIDE GAME LOGIC GOES IN HERE
     This function will be called roughly every [tickTime] milliseconds, and it'll allow us to have an asynchronous game "loop" so it's going to be used to handle all the non-I/O logic.
   */
-  
+
   /* SERVER-SIDE GAME LOGIC GOES IN HERE*/
- 
+
   //Set timer to loop. This isn't technically recursion, as I understand it.
   setTimeout(tickServer, tickTime);
 }
@@ -85,18 +89,26 @@ function startGameLoop() {
 
 
 mongoClient.connect(mongoURL, function(err, client) {
-  if (err){
-    console.log("mongoURL:: " , mongoURL);
-    throw err;
-  }
-  db = client.db(mongoDBName);
-  startGameLoop();
-  app.listen(port, function () {
-    console.log("== Server is listening on port", port);
-    const timeoutScheduled = Date.now();
-    
-  });
-  
+    if (err){
+        console.log("mongoURL:: " , mongoURL);
+        throw err;
+    }
+
+    db = client.db(mongoUser);
+    console.log(db);
+    var itemCollection = db.collection('items');
+    itemCollection.find({}).toArray(function(err, result){
+        if(err)
+            throw err;
+        itemList = result;
+    });
+
+
+    startGameLoop();
+    app.listen(port, function () {
+        console.log("== Server is listening on port", port);
+        const timeoutScheduled = Date.now();
+    });
 });
 
 
