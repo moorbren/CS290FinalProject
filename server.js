966ef0023c498092cb8ab5382863379e4b43c229
bonetrail <moorbren@oregonstate.edu>
2018-11-30 19:58:02 -0800

    More css

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var mongoDBName = process.env.MONGO_DB_NAME;
 var mongoURL =
 	'mongodb://' + mongoUser + ':' + mongoPassword + '@' +
   mongoHost + ':' + mongoPort + '/' + mongoDBName;
-  
+
 var db;
 
 
@@ -27,7 +27,7 @@ var port = process.env.PORT || 3000;
 
 var itemsArray;
 
-var availablePages = ['home', 'store', 'supplies', 'crafting']; 
+var availablePages = ['home', 'store', 'supplies', 'crafting'];
 
 app.use(express.static('public'));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -48,20 +48,8 @@ app.get('/store', function(req, res, next){
       throw err;
     }
     userInfo=usr[0];
-
-
-
-    res.status(200).render("store", {
-      Username: userInfo.name,
-      earnings: userInfo.totalEarnings,
-      bank: userInfo.cash,
-      joinDate: userInfo.start,
-    } );
+    res.status(200).render("store", userInfo);
   });
-
-
-
-
 });
 
 app.get('/supplies', function(req, res, next){
@@ -81,12 +69,12 @@ app.get('/:section', function(req, res, next){
   var section = req.params.section.toLowerCase();
   if (availablePages.indexOf(section) > -1) {
     console.log("==section found::"+section);
-    
+
     res.status(200).render(req.params.section);
   }
   else {
     console.log("==404 section not found::"+req.params.section);
-    next(); 
+    next();
   }
 });
 
@@ -101,7 +89,7 @@ var tickTime = 2500; //milliseconds between "frames"
 
 //The server-side logic will happen inside this function.
 function tickServer() {
-  
+
   //deltaTime calculations
   //realistically, I'm not sure how much the milliseconds in deltaTime is really going to matter, but it's generally a good idea to put that in game loops in case you ever need it.
   var newTime = Date.now();
@@ -112,9 +100,9 @@ function tickServer() {
   /* SERVER-SIDE GAME LOGIC GOES IN HERE
     This function will be called roughly every [tickTime] milliseconds, and it'll allow us to have an asynchronous game "loop" so it's going to be used to handle all the non-I/O logic.
   */
-  
+
   /* SERVER-SIDE GAME LOGIC GOES IN HERE*/
- 
+
   //Set timer to loop. This isn't technically recursion, as I understand it.
   setTimeout(tickServer, tickTime);
 }
@@ -139,9 +127,9 @@ mongoClient.connect(mongoURL, function(err, client) {
     app.listen(port, function () {
       console.log("== Server is listening on port", port);
       const timeoutScheduled = Date.now();
-      
+
     });
-  
+
   });
 });
 
@@ -162,6 +150,3 @@ app.get('/posts/:postID', function(req,res,next){
   }
 });
 */
-
-
-
